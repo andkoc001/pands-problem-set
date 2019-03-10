904d909b62f1589b047f4703df6a8d1e3f2ef68f
andkoc001 <and.koc001@gmail.com>
2019-03-10 20:26:11 +0000

    Update to solution to probilem 7 - approx. precision implemented.

--- 07-squareroot.py
+++ 07-squareroot.py
@@ -11,11 +11,15 @@
 # Asking for user's input and assigns float type to the input
 number = float(input("Give enter a positive number: "))
 
-# Intermediate check - prints out user's entry
-print(number)
-
 # Calculate square root of the entered number
-sqrt = number**0.5
+sqr = number**0.5
 
 # Intermediate check - prints out square root
-print(sqrt)
+# print(sqr)
+
+# Approximation to 1 precision digit after floating point
+# Verbatim from https://stackoverflow.com/a/50038023
+approx = float("%0.1f" % (sqr))
+
+# Print out approxmate value of the square root
+print("The square root of ", number, " is approx.", approx)
