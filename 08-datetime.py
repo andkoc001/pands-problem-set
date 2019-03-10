2a19da80f505d0ea75684db0e7d30f29c914eeb8
andkoc001 <and.koc001@gmail.com>
2019-03-10 21:46:30 +0000

    Update - week name and month name show up.

--- 08-datetime.py
+++ 08-datetime.py
@@ -7,3 +7,58 @@
 # Last update: 10-03-2019
 
 ###
+
+# External libraires
+import datetime as dt
+
+# Weekday - assigns day name to day number
+week_day = dt.datetime.today().weekday()
+if week_day == 0:
+    week_day_name = "Monday,"
+elif week_day == 1:
+    week_day_name = "Tuesday,"
+elif week_day == 2:
+    week_day_name = "Wednesday,"
+elif week_day == 3:
+    week_day_name = "Thursday,"
+elif week_day == 4:
+    week_day_name = "Friday,"
+elif week_day == 5:
+    week_day_name = "Saturday,"
+elif week_day == 6:
+    week_day_name = "Sunday,"
+# print(week_day_name)
+
+
+# Weekday - assigns day name to day number
+mn = dt.datetime.today().month
+if mn == 1:
+    month_name = "January"
+elif mn == 2:
+    month_name = "February"
+elif mn == 3:
+    month_name = "March"
+elif mn == 4:
+    month_name = "April"
+elif mn == 5:
+    month_name = "May"
+elif mn == 6:
+    month_name = "June"
+elif mn == 7:
+    month_name = "July"
+elif mn == 8:
+    month_name = "August"
+elif mn == 9:
+    month_name = "September"
+elif mn == 10:
+    month_name = "October"
+elif mn == 11:
+    month_name = "November"
+elif mn == 12:
+    month_name = "December"
+# print(month_name)
+
+# test
+print(week_day_name, month_name)
+
+print(dt.datetime.now())
