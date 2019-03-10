e8038fd054f92b66b759a26010be4096118c3535
andkoc001 <and.koc001@gmail.com>
2019-03-10 20:57:03 +0000

    Creation of empty file of solution to problem 8.

---
+++ 08-datetime.py
@@ -0,0 +1,9 @@
+# Title: Date time
+# Description: Solution to problem 8 - program that outputs today’s date and time in the format ”Monday, January 10th 2019 at 1:15pm”.
+# Context: Programming and Scripting, GMIT, 2019
+# Author: Andrzej Kocielski
+# Email: G00376291@gmit.ie
+# Date of creation: 10-03-2019
+# Last update: 10-03-2019
+
+###
