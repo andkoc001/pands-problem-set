335146e9499559ad435cdc1b749ead4213addd36
andkoc001 <and.koc001@gmail.com>
2019-03-10 19:42:46 +0000

    Solution to probilem 7 created - empty file.

---
+++ 07-squareroot.py
@@ -0,0 +1,11 @@
+# Title: Squre root
+# Description: Solution to problem 7 - program that takes a positive floating point number as input and outputs an approximation of its square root.
+# Context: Programming and Scripting, GMIT, 2019
+# Author: Andrzej Kocielski
+# Email: G00376291@gmit.ie
+# Date of creation: 10-03-2019
+# Last update: 10-03-2019
+
+###
+
+#
