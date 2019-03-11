e20c1072dcf4a0b9c3b14da63544420f84839ab9
andkoc001 <and.koc001@gmail.com>
2019-03-11 21:57:55 +0000

    Update - today's day as ordinal number added.

--- 08-datetime.py
+++ 08-datetime.py
@@ -11,26 +11,26 @@
 # External libraires
 import datetime as dt
 
-# Weekday - assigns day name to day number
+# Weekday - assigns day name (week_day_name) to day number (week_day)
 week_day = dt.datetime.today().weekday()
 if week_day == 0:
-    week_day_name = "Monday,"
+    week_day_name = "Monday"
 elif week_day == 1:
-    week_day_name = "Tuesday,"
+    week_day_name = "Tuesday"
 elif week_day == 2:
-    week_day_name = "Wednesday,"
+    week_day_name = "Wednesday"
 elif week_day == 3:
-    week_day_name = "Thursday,"
+    week_day_name = "Thursday"
 elif week_day == 4:
-    week_day_name = "Friday,"
+    week_day_name = "Friday"
 elif week_day == 5:
-    week_day_name = "Saturday,"
+    week_day_name = "Saturday"
 elif week_day == 6:
-    week_day_name = "Sunday,"
-# print(week_day_name)
+    week_day_name = "Sunday"
+# print(week_day_name) # intermediate test
 
 
-# Weekday - assigns day name to day number
+# Weekday - assigns month name (month_name) to month number (mn)
 mn = dt.datetime.today().month
 if mn == 1:
     month_name = "January"
@@ -56,9 +56,28 @@ elif mn == 11:
     month_name = "November"
 elif mn == 12:
     month_name = "December"
-# print(month_name)
+# print(month_name) # intermediate test
 
 # test
-print(week_day_name, month_name)
+# print(week_day_name, month_name)
 
-print(dt.datetime.now())
+# definition of today's day as ordinal number
+today_day = dt.datetime.today().day
+
+if today_day == 1 or today_day == 21 or today_day == 31:
+    today_ordinal = str(today_day) + "st"
+elif today_day == 2 or today_day == 22:
+    today_ordinal = str(today_day) + "nd"
+elif today_day == 3 or today_day == 23:
+    today_ordinal = str(today_day) + "rd"
+else:
+    today_ordinal = str(today_day) + "th"
+
+# print(today_ordinal) # intermediate test
+
+# definition of a new variable holding the day and time in requested format
+# formatting with the use of f"{}" notation
+full = f"{week_day_name}, {month_name} {today_ordinal}"
+print(full)
+
+# print(dt.datetime.now())
