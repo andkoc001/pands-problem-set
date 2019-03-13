ec7ff8ba1bf6fbaf130f603186858b57eab19605
andkoc001 <and.koc001@gmail.com>
2019-03-13 08:07:07 +0000

    Solution to problem 09 - just starting.

---
+++ 09-second.py
@@ -0,0 +1,12 @@
+# Title: Second
+# Description: Solution to problem 9 - program that reads in a text file and outputs every second line.
+# The program should take the filename from an argument on the command line.
+# Context: Programming and Scripting, GMIT, 2019
+# Author: Andrzej Kocielski
+# Email: G00376291@gmit.ie
+# Date of creation: 13-03-2019
+# Last update: 13-03-2019
+
+###
+
+#
