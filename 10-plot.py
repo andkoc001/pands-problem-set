a441d6bce2d534afb62b1b2648809c24e8751357
andkoc001 <and.koc001@gmail.com>
2019-03-15 23:32:05 +0000

    Solution to problem 10 - empty file created.

---
+++ 10-plot.py
@@ -0,0 +1,9 @@
+# Title: Plot of funtions
+# Description: Solution to problem 10 - program that displays a plot of the functions x, x 2 and 2 x in the range [0, 4].
+# Context: Programming and Scripting, GMIT, 2019
+# Author: Andrzej Kocielski
+# Email: G00376291@gmit.ie
+# Date of creation: 15-03-2019
+# Last update: 15-03-2019
+
+###
